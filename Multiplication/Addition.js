6982db46765528b971341d1f07c0d7e9036c0870
Vivek Singh <iamvivekrajawat@gmail.com>
2025-05-06 12:04:09 +0530

    Mathematical Operation

---
+++ Multiplication/Addition.js
@@ -0,0 +1,11 @@
+const data = document.getElementById('btn')
+data.addEventListener('click',()=>{
+const firstNumber = document.getElementById('frst')
+const input1 = Number(firstNumber.value)
+const secondNumber  = document.getElementById('scnd')
+const input2 = Number(secondNumber.value)
+// const rslt = document.querySelector('p')
+const r = input1*input2
+const rslt = document.getElementById("para")
+rslt.innerHTML = "Result : "+r
+})
